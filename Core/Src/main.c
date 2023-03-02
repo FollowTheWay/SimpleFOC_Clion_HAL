442c3343b357e290e094a368fd8ee45c700b0e37
FollowTheWay <1935595312@qq.com>
2023-03-02 19:08:21 +0800

    移植完成，初始化完成

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -70,75 +70,75 @@ void SystemClock_Config (void);
   */
 int main (void)
 {
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
 
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init ();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init ();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config ();
+    /* Configure the system clock */
+    SystemClock_Config ();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init ();
-  MX_USART2_UART_Init ();
-  /* USER CODE BEGIN 2 */
-  RetargetInit (&huart2);
-  HAL_Delay (100);
-  sensor_init ();
-  LPF_init ();
-  PID_init ();
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init ();
+    MX_USART2_UART_Init ();
+    /* USER CODE BEGIN 2 */
+    RetargetInit (&huart2);
+    HAL_Delay (100);
+    sensor_init ();
+    LPF_init ();
+    PID_init ();
 
-  voltage_power_supply = 12;          // V 电源电压
-  pole_pairs = 10;                  // 电机极对数，按照实际设置，虽然可以上电检测但有失败的概率
-  voltage_sensor_align = 2;          // V 航模电机设置的值小一点比如0.5-1，云台电机设置的大一点比如2-3
-  voltage_limit = 6;                  // V，主要为限制电机最大电流，最大值需小于12/1.732=6.9
-  velocity_limit = 20;              // rad/s 角度模式时限制最大转速，力矩模式和速度模式不起作用
-  current_limit = 50;                  // A，foc_current和dc_current模式限制电流，不能为0。速度模式和位置模式起作用
-  torque_controller = Type_voltage; // 当前只有电压模式
-  controller = Type_velocity;          // Type_angle; //Type_torque; //Type_velocity
-  PID_velocity.P = 0.5;              // 0.5, 速度环PI参数，只用P参数方便快速调试
-  PID_velocity.I = 0.2;              // 0.2
-  P_angle.P = 20;                      // 位置环参数，只需P参数，一般不需要改动
-  PID_velocity.output_ramp = 20;      // 速度爬升斜率，如果不需要可以设置为0
-  LPF_velocity.Tf = 0.0001;          // Tf设置小一点，配合爬升斜率设置，速度切换更平稳；如果没有爬升模式的斜率限制，Tf太小电机容易抖动，可设置为0.02。
-  target = 0;
+    voltage_power_supply = 12;          // V 电源电压
+    pole_pairs = 10;                  // 电机极对数，按照实际设置，虽然可以上电检测但有失败的概率
+    voltage_sensor_align = 2;          // V 航模电机设置的值小一点比如0.5-1，云台电机设置的大一点比如2-3
+    voltage_limit = 6;                  // V，主要为限制电机最大电流，最大值需小于12/1.732=6.9
+    velocity_limit = 20;              // rad/s 角度模式时限制最大转速，力矩模式和速度模式不起作用
+    current_limit = 50;                  // A，foc_current和dc_current模式限制电流，不能为0。速度模式和位置模式起作用
+    torque_controller = Type_voltage; // 当前只有电压模式
+    controller = Type_velocity;          // Type_angle; //Type_torque; //Type_velocity
+    PID_velocity.P = 0.5;              // 0.5, 速度环PI参数，只用P参数方便快速调试
+    PID_velocity.I = 0.2;              // 0.2
+    P_angle.P = 20;                      // 位置环参数，只需P参数，一般不需要改动
+    PID_velocity.output_ramp = 20;      // 速度爬升斜率，如果不需要可以设置为0
+    LPF_velocity.Tf = 0.0001;          // Tf设置小一点，配合爬升斜率设置，速度切换更平稳；如果没有爬升模式的斜率限制，Tf太小电机容易抖动，可设置为0.02。
+    target = 0;
 
-  Motor_init ();
-  Motor_initFOC (0, CCW); //(0,UNKNOWN);  //(1.1,CW); 第一次先获得偏移角和方向，填入代码编译后再下载，以后可以跳过零点校准
-  printf ("Motor ready.\r\n");
-  /* USER CODE END 2 */
+    Motor_init ();
+    Motor_initFOC (0, CCW); //(0,UNKNOWN);  //(1.1,CW); 第一次先获得偏移角和方向，填入代码编译后再下载，以后可以跳过零点校准
+    printf ("Motor ready.\r\n");
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  /***1000 10ms***/
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    /***1000 10ms***/
 // HAL_RCC_GetHCLKFreq()/1000 1ms中断一次，即HAL_Delay函数延时基准为1ms
 // HAL_RCC_GetHCLKFreq()/100000  10us中断一次，即HAL_Delay函数延时基准为10us
 // HAL_RCC_GetHCLKFreq()/1000000 1us中断一次，即HAL_Delay函数延时基准为1us
-  HAL_SYSTICK_Config (HAL_RCC_GetHCLKFreq () / 1000000);  // 配置并启动系统滴答定时器
-  while (1)
-    {
+    HAL_SYSTICK_Config (HAL_RCC_GetHCLKFreq () / 1000000);  // 配置并启动系统滴答定时器
+    while (1)
+        {
 //      printf ("%d\r\n", getRawCount ());
 
-      /* USER CODE END WHILE */
+            /* USER CODE END WHILE */
 
-      /* USER CODE BEGIN 3 */
+            /* USER CODE BEGIN 3 */
 //        HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
 //        HAL_Delay(10000);
-    }
-  /* USER CODE END 3 */
+        }
+    /* USER CODE END 3 */
 }
 
 /**
@@ -147,43 +147,43 @@ int main (void)
   */
 void SystemClock_Config (void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Configure the main internal regulator output voltage
+    */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig (&RCC_OscInitStruct) != HAL_OK)
-    {
-      Error_Handler ();
-    }
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 4;
+    RCC_OscInitStruct.PLL.PLLN = 168;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+    if (HAL_RCC_OscConfig (&RCC_OscInitStruct) != HAL_OK)
+        {
+            Error_Handler ();
+        }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                                | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig (&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-    {
-      Error_Handler ();
-    }
+    if (HAL_RCC_ClockConfig (&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+        {
+            Error_Handler ();
+        }
 }
 
 /* USER CODE BEGIN 4 */
@@ -196,13 +196,13 @@ void SystemClock_Config (void)
   */
 void Error_Handler (void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq ();
-  while (1)
-    {
-    }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq ();
+    while (1)
+        {
+        }
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
--- SimpleFOC/sensor/sensor.c
+++ SimpleFOC/sensor/sensor.c
@@ -6,52 +6,52 @@
 /******************************************************************************/
 long cpr;
 long velocity_calc_timestamp; // 速度计时，用于计算速度
-long angle_data_prev;		  // 获取角度用
-float angle_prev;			  // 获取速度用
-float full_rotation_offset;	  // 角度累加
+long angle_data_prev;          // 获取角度用
+float angle_prev;              // 获取速度用
+float full_rotation_offset;      // 角度累加
 /******************************************************************************/
-void sensor_init(void)
+void sensor_init (void)
 {
-    IIC_GPIO_Config();// AS5600
+    IIC_GPIO_Config ();// AS5600
     cpr = AS5600_CPR;
-    printf("sensor: AS5600\r\n");
+    printf ("sensor: AS5600\r\n");
 
-    HAL_Delay(100);//10ms
-    angle_data_prev = getRawCount();
+    HAL_Delay (100);//10ms
+    angle_data_prev = getRawCount ();
     full_rotation_offset = 0;
     velocity_calc_timestamp = 0; // SysTick->VAL;
-    HAL_Delay(50);//5ms
-    angle_prev = getAngle();
+    HAL_Delay (50);//5ms
+    angle_prev = getAngle ();
 }
 
 /******************************************************************************/
 //当前角度——上一次保存下来的角度 offset是圈数+角度
-float getAngle(void)
+float getAngle (void)
 {
     long angle_data, d_angle;
 
-    angle_data = getRawCount(); // 获取编码器的原始值
+    angle_data = getRawCount (); // 获取编码器的原始值
 
     // tracking the number of rotations 跟踪旋转次数
     // in order to expand angle range form [0,2PI] to basically infinity 为了将角度范围从[0,2PI]扩展到基本无穷大
     d_angle = angle_data - angle_data_prev;
     // if overflow happened track it as full rotation 如果发生溢出，请将其跟踪为完全旋转
-    if (fabs(d_angle) > (0.8f * cpr))
+    if (fabs (d_angle) > (0.8f * cpr))
         full_rotation_offset += (d_angle > 0) ? -_2PI : _2PI;
     // save the current angle value for the next steps 保存当前角度值以供后续步骤使用
     // in order to know if overflow happened 以便知道是否发生溢出
     angle_data_prev = angle_data; // 更新上一次的编码器原始值
 
     if (full_rotation_offset >= (_2PI * 2000)) // 转动圈数过多后浮点数精度下降，电流增加并最终堵转，每隔一定圈数归零一次
-    {										   // 这个问题针对电机长时间连续一个方向转动
-        full_rotation_offset = 0;			   // 速度模式，高速转动时每次归零会导致电机抖动一次
-        angle_prev = angle_prev - _2PI * 2000;
-    }
+        {                                           // 这个问题针对电机长时间连续一个方向转动
+            full_rotation_offset = 0;               // 速度模式，高速转动时每次归零会导致电机抖动一次
+            angle_prev = angle_prev - _2PI * 2000;
+        }
     if (full_rotation_offset <= (-_2PI * 2000))
-    {
-        full_rotation_offset = 0;
-        angle_prev = angle_prev + _2PI * 2000;
-    }
+        {
+            full_rotation_offset = 0;
+            angle_prev = angle_prev + _2PI * 2000;
+        }
 
     // return the full angle
     // (number of full rotations)*2PI + current sensor angle
@@ -60,7 +60,7 @@ float getAngle(void)
 
 /******************************************************************************/
 // Shaft velocity calculation
-float getVelocity(void)
+float getVelocity (void)
 {
     long now_us;
     float Ts, angle_now, vel;
@@ -68,15 +68,15 @@ float getVelocity(void)
     // calculate sample time
     now_us = SysTick->VAL;
     if (now_us < velocity_calc_timestamp)
-        Ts = (float)(velocity_calc_timestamp - now_us) / 21 * 1e-6f;
+        Ts = (float) (velocity_calc_timestamp - now_us) / 21 * 1e-6f;
     else
-        Ts = (float)(0xFFFFFF - now_us + velocity_calc_timestamp) / 21 * 1e-6f;
+        Ts = (float) (0xFFFFFF - now_us + velocity_calc_timestamp) / 21 * 1e-6f;
     // quick fix for strange cases (micros overflow)
     if (Ts == 0 || Ts > 0.5f)
         Ts = 1e-3f;
 
     // current angle
-    angle_now = getAngle();
+    angle_now = getAngle ();
     // velocity calculation
     vel = (angle_now - angle_prev) / Ts;
 
